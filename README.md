a5fe105c87ec7af63557acb2f8718b739dd955de
ivanababec <7085675925ib@gmail.com>
2024-05-23 15:02:14 -0500

    added size

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 ### Please copy below code
 ```
 module "testns" {
-  source = "ivanababec/namespace/kubernetes"
-  name   = "testns"
+  source           = "ivanababec/namespace/kubernetes"
+  name             = "testns"
+  pod_quota        = 50
+  pod_cpu_limit    = "2096m"
+  pod_memory_limit = "4Gi"
   annotations = {
     new = "application"
   }
--- variable.tf
+++ variable.tf
@@ -6,12 +6,30 @@ variable "name" {
 
 variable "annotations" {
   description = "Please provide annotations"
-  type        = map(any)
+  type        = map
   default     = {}
 }
 
 variable "labels" {
   description = "Please provide labels"
-  type        = map(any)
+  type        = map
   default     = {}
+}
+
+variable "pod_quota" {
+  description = "Please specify pod quota"
+  type        = string
+  default     = ""
+}
+
+variable "pod_cpu_limit" {
+  description = "Please specify cpu limit"
+  type        = string
+  default     = ""
+}
+
+variable "pod_memory_limit" {
+  description = "Please specify memory limit"
+  type        = string
+  default     = ""
 }
